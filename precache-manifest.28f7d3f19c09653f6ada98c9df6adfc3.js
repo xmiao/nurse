41bac28ef2ecdc1dfda065940eeb8ec784a61b92
Xijiang <mxj@winning.com.cn>
2019-11-20 17:41:39 +0800

    Updates

---
+++ precache-manifest.28f7d3f19c09653f6ada98c9df6adfc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e11bd36f4cfe2b8491c28ab3579d8e",
+    "url": "/nurse/index.html"
+  },
+  {
+    "revision": "c0c1f18e512b0326fbf4",
+    "url": "/nurse/static/css/main.da5ac013.chunk.css"
+  },
+  {
+    "revision": "86b065a5711c9cf2852c",
+    "url": "/nurse/static/js/2.2d120ef8.chunk.js"
+  },
+  {
+    "revision": "c0c1f18e512b0326fbf4",
+    "url": "/nurse/static/js/main.852fa06c.chunk.js"
+  },
+  {
+    "revision": "3faff497cd1701e275bf",
+    "url": "/nurse/static/js/runtime-main.6e7b76b0.js"
+  }
+]);
